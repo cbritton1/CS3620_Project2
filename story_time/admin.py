6b6ae21810af409f84c1bbdf004267f2dd5fb261
Cory Britton <corybritton@mail.weber.edu>
2020-10-15 18:37:35 -0600

    deleting data

--- story_time/admin.py
+++ story_time/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import StoryOne, StoryTwo, StoryWar, StoryDragons, StoryWalmart
+from .models import StoryOne, StoryTwo, StoryWar, StoryDragons, StoryWalmart,\
+    StoryLoveLetter, StorySmellyCat, StoryGreetingsEarthlings
 
 
 # Register your models here.
@@ -8,3 +9,6 @@ admin.site.register(StoryTwo)
 admin.site.register(StoryWar)
 admin.site.register(StoryDragons)
 admin.site.register(StoryWalmart)
+admin.site.register(StoryLoveLetter)
+admin.site.register(StorySmellyCat)
+admin.site.register(StoryGreetingsEarthlings)
